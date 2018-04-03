65dd42b396ef9c63a26b0afa48c34a6205179f85
knaito <knaito@asial.co.jp>
2018-04-03 06:37:20 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-ons-page>
+      <v-ons-toolbar>
+        <div class="center">{{ title }}</div>
+        <div class="right">
+          <v-ons-toolbar-button>
+            <v-ons-icon icon="ion-navicon, material: md-menu"></v-ons-icon>
+          </v-ons-toolbar-button>
+        </div>
+      </v-ons-toolbar>
+      <div style="text-align: center; padding-top:10px">Hello World!</div>
+      <p style="text-align: center">
+        <v-ons-button @click="alert">Click Me!!!!</v-ons-button>
+      </p>
+      123
+      <component :is="page" v-for="page in messages">aaa</component>
+    </v-ons-page>
+</template>
+<script>
+  export default{
+    data() {
+      return {
+        title: 'My app',
+        messages: ['good-morning', 'hello', 'good-bye']
+      };
+    },
+    components: {
+      'good-morning': {
+        template: '<div>Good Morning</div>'
+      },
+      'hello': {
+        template: '<div>Hello!</div>'
+      },
+      'good-bye': {
+        template: '<div>Good-Bye!</div>'
+      }
+    },
+    methods: {
+      alert() {
+        this.$ons.notification.alert('This is an Onsen UI alert notification test.');
+      }
+    }
+  };
+</script>
